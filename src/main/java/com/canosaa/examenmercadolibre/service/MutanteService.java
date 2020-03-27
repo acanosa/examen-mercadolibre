5a39d491efeff025c7517649ff23d6ed5ead91c6
Alejandro Canosa <ale.canosa19@gmail.com>
2020-03-27 01:49:21 -0300

    Refactor MutanteService

--- src/main/java/com/canosaa/examenmercadolibre/service/MutanteService.java
+++ src/main/java/com/canosaa/examenmercadolibre/service/MutanteService.java
@@ -23,11 +23,20 @@ public class MutanteService {
         Assert.notEmpty(adn, "La cadena de ADN es obligatoria");
 
         int cantidadSecuencias = 0;
-        int caracterIterado = 0;
+        cantidadSecuencias = analizarAdn(adn);
+        boolean esMutante = cantidadSecuencias > 1;
+        
+        guardarTestDeAdn(adn, esMutante);
+        
+        return esMutante;
+    }
+
+    private int analizarAdn(String[] adn) throws IllegalArgumentException {
+        int indiceCaracterIterado = 0;
+        int cantidadSecuencias = 0;
         int cantidadFilas = adn.length;
         Pattern pattern = Pattern.compile("[A]{4}|[T]{4}|[G]{4}|[C]{4}");
         for (int i = 0; i < cantidadFilas; i++) {
-            //horizontal
             Matcher matcher = pattern.matcher(adn[i]);
             if (matcher.find()) {
                 cantidadSecuencias++;
@@ -36,49 +45,74 @@ public class MutanteService {
                 }
             }
             int longitudPalabra = adn[i].length();
-            if (adn[i].length() != cantidadFilas) {
+            
+            if (laPalabraTieneLongitudDistintaALaCantidadDeFilas(adn, i, cantidadFilas)) {
                 throw new IllegalArgumentException("Todas las cadenas de caracteres deben tener una longitud "
                         + "igual a la cantidad de cadenas que tiene el ADN para determinar la validez del mutante");
             }
 
-            while (caracterIterado < longitudPalabra) {
-                //vertical
-                if ((i + 3) < cantidadFilas) {
-                    if (adn[i].toCharArray()[caracterIterado] == adn[i + 1].toCharArray()[caracterIterado]
-                            && adn[i].toCharArray()[caracterIterado] == adn[i + 2].toCharArray()[caracterIterado]
-                            && adn[i].toCharArray()[caracterIterado] == adn[i + 3].toCharArray()[caracterIterado]) {
-                        cantidadSecuencias++;
-//                        diagonal    
-                    }
-                    if (caracterIterado + 3 < longitudPalabra) {
-                        if (adn[i].toCharArray()[caracterIterado] == adn[i + 1].toCharArray()[caracterIterado + 1]
-                                && adn[i].toCharArray()[caracterIterado] == adn[i + 2].toCharArray()[caracterIterado + 2]
-                                && adn[i].toCharArray()[caracterIterado] == adn[i + 3].toCharArray()[caracterIterado + 3]) {
-                            cantidadSecuencias++;
-                        }
-                    }
-
+            while (indiceCaracterIterado < longitudPalabra) {
+                if (hay4FilasOMasPorRecorrer(i, cantidadFilas)) {
+                    cantidadSecuencias = buscarCaracteresIgualesVertical(adn, i, indiceCaracterIterado, cantidadSecuencias);
+                    cantidadSecuencias = buscarCaracteresIgualesDiagonalDerecha(indiceCaracterIterado, longitudPalabra, adn, i, cantidadSecuencias);
                 }
-                if (caracterIterado - 3 > 0 && i + 3 < cantidadFilas) {
-                    if (adn[i].toCharArray()[caracterIterado] == adn[i + 1].toCharArray()[caracterIterado - 1]
-                            && adn[i].toCharArray()[caracterIterado] == adn[i + 2].toCharArray()[caracterIterado - 2]
-                            && adn[i].toCharArray()[caracterIterado] == adn[i + 3].toCharArray()[caracterIterado - 3]) {
-                        cantidadSecuencias++;
-                    }
+                if (hay4CaracteresOMasALaIzquierda(indiceCaracterIterado) && hay4FilasOMasPorRecorrer(i, cantidadFilas)) {
+                    cantidadSecuencias = buscarCaracteresIgualesDiagonalIzquierda(adn, i, indiceCaracterIterado, cantidadSecuencias);
                 }
                 if (cantidadSecuencias > 1) {
                     break;
                 }
-                caracterIterado++;
+                indiceCaracterIterado++;
             }
-            caracterIterado = 0;
+            indiceCaracterIterado = 0;
         }
-        boolean esMutante = cantidadSecuencias > 1;
-        
+        return cantidadSecuencias;
+    }
+
+    private void guardarTestDeAdn(String[] adn, boolean esMutante) {
         Mutante mutante = new Mutante(String.join(", ", adn), esMutante);
         mutanteRepository.save(mutante);
-        
-        return esMutante;
+    }
+
+    private static boolean hay4CaracteresOMasALaIzquierda(int caracterIterado) {
+        return caracterIterado - 3 > 0;
+    }
+
+    private static boolean laPalabraTieneLongitudDistintaALaCantidadDeFilas(String[] adn, int i, int cantidadFilas) {
+        return adn[i].length() != cantidadFilas;
+    }
+
+    private int buscarCaracteresIgualesDiagonalIzquierda(String[] adn, int i, int caracterIterado, int cantidadSecuencias) {
+        if (adn[i].toCharArray()[caracterIterado] == adn[i + 1].toCharArray()[caracterIterado - 1]
+                && adn[i].toCharArray()[caracterIterado] == adn[i + 2].toCharArray()[caracterIterado - 2]
+                && adn[i].toCharArray()[caracterIterado] == adn[i + 3].toCharArray()[caracterIterado - 3]) {
+            cantidadSecuencias++;
+        }
+        return cantidadSecuencias;
+    }
+
+    private static boolean hay4FilasOMasPorRecorrer(int i, int cantidadFilas) {
+        return (i + 3) < cantidadFilas;
+    }
+
+    private int buscarCaracteresIgualesVertical(String[] adn, int i, int caracterIterado, int cantidadSecuencias) {
+        if (adn[i].toCharArray()[caracterIterado] == adn[i + 1].toCharArray()[caracterIterado]
+                && adn[i].toCharArray()[caracterIterado] == adn[i + 2].toCharArray()[caracterIterado]
+                && adn[i].toCharArray()[caracterIterado] == adn[i + 3].toCharArray()[caracterIterado]) {
+            cantidadSecuencias++;
+        }
+        return cantidadSecuencias;
+    }
+
+    private int buscarCaracteresIgualesDiagonalDerecha(int caracterIterado, int longitudPalabra, String[] adn, int i, int cantidadSecuencias) {
+        if (caracterIterado + 3 < longitudPalabra) {
+            if (adn[i].toCharArray()[caracterIterado] == adn[i + 1].toCharArray()[caracterIterado + 1]
+                    && adn[i].toCharArray()[caracterIterado] == adn[i + 2].toCharArray()[caracterIterado + 2]
+                    && adn[i].toCharArray()[caracterIterado] == adn[i + 3].toCharArray()[caracterIterado + 3]) {
+                cantidadSecuencias++;
+            }
+        }
+        return cantidadSecuencias;
     }
     
     public EstadisticaTestMutantes obtenerEstadisticas(){
